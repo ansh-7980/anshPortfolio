efd7c4d94072bbac81e737746909da82c2dcdc6e
ansh-7980 <anshsharma7980@gmail.com>
2023-07-11 17:48:43 +0530

    Portfolio web

---
+++ src/constants/index.js
@@ -0,0 +1,291 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  firebase,
+  enact,
+  jd,
+  hometown,
+  babysitting,
+  zwigato
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Frontend Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  // {
+  //   name: "TypeScript",
+  //   icon: typescript,
+  // },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+  {
+    name: "firebase",
+    icon: firebase,
+  },
+];
+
+const experiences = [
+  // {
+  //   title: "React.js Developer",
+  //   company_name: "Starbucks",
+  //   icon: starbucks,
+  //   iconBg: "#383E56",
+  //   date: "March 2020 - April 2021",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "JD Web Services",
+  //   icon: jd,
+  //   iconBg: "#383E56",
+  //   date: "March 2021 - April 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using HTML, CSS, Bootstrap, and JavaScript and other related technologies.",
+  //     "I've specialized in building responsive frontends for websites and have also gained proficiency in PHP and MySQL.",
+  //     "I've worked on a number of projects, including a hospital website, a school website, and a Canadian transport service website.",
+  //     "I've been responsible for designing and implementing the frontend, ensuring that the websites are user-friendly, visually appealing, and accessible on different devices.",
+  //     "I've also collaborated with backend developers to integrate frontend and backend functionalities seamlessly.",
+  //     "I'm a detail-oriented developer who takes pride in delivering high-quality work, and I'm always eager to learn new technologies and approaches to improve my skills."
+  //   ],
+  // },
+  {
+    title: "React Native Developer",
+    company_name: "enAct eServices Pvt Ltd.",
+    icon: enact,
+    iconBg: "#383E56",
+    date: "Feb 2023 - Present",
+    points: [
+      "I specialize in building cross-platform mobile applications using JavaScript and React.",
+      "I have a strong understanding of the core concepts of React Native and how to use them to build scalable, performant, and intuitive mobile apps.",
+      "I'm well-versed in various libraries, tools, and frameworks commonly used in React Native development, such as Redux, React Navigation, and Expo.",
+      "I've worked on a number of projects, including a Food Delivery application, a parcel Delivery application, and a Baby sitting application."
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  
+  {
+    name: "Zwigato",
+    description:
+      "Web application that enables a peer-to-peer delivery service offered by enAct eServices, where users can send and receive packages and documents within a specified area.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: zwigato,
+    source_code_link: "https://github.com/ansh-7980",
+  },
+  {
+    name: "Baby Sitting",
+    description:
+      "A babysitting app connects parents with qualified babysitters, enabling easy booking, scheduling, and communication for reliable and convenient childcare services.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: babysitting,
+    source_code_link: "https://github.com/ansh-7980",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
